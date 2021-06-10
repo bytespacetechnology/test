4638a6af3f1e87eefec5f78fc8b6053c360b1089
rabindra2020 <56604049+rabindra2020@users.noreply.github.com>
2021-06-10 15:46:27 +0545

    first Commit

---
+++ crudapi/library/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from library.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Book
+        fields=['id','title','author','isbn','publisher']
